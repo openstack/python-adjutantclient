openstack/python-adjutantclient

commit b89a07b5a16cfabc80a8e6d26e003066b4418524

Remove six
Remove six.moves Replace the following items with Python 3 style code.

- six.moves.urllib
- six.iteritems

Change-Id: I65d7a43ac9ae76e0634b72c6bb54cb3a98bd7aab

adjutantclient/v1/users.py
@@ -13,7 +13,7 @@
 #    under the License.
 
 
-from six.moves.urllib import parse
+from moves.urllib import parse
 
 from adjutantclient.common import base
 

adjutantclient/common/base.py
@@ -21,11 +21,10 @@ Base utilities to build API operation managers and objects on top of.
 """
 import abc
 import copy
-import six
 
 from oslo_utils import reflection
 from oslo_utils import strutils
-from six.moves.urllib import parse
+from urllib import parse
 
 from adjutantclient._i18n import _
 from adjutantclient import exc as exceptions
@@ -207,8 +206,7 @@ class BaseManager(HookableMixin):
         return self.client.delete(url)
 
 
-@six.add_metaclass(abc.ABCMeta)
-class ManagerWithFind(BaseManager):
+class ManagerWithFind(BaseManager, metaclass=abc.ABCMeta):
     """Manager with additional `find()`/`findall()` methods."""
 
     @abc.abstractmethod

adjutantclient/osc/v1/quota.py
@@ -13,7 +13,6 @@
 #    under the License.
 
 import logging
-import six
 
 from osc_lib.command import command
 from osc_lib.i18n import _
@@ -61,9 +60,8 @@ class QuotaShow(command.Lister):
             region = quota_data['regions'][0]
 
             rows = []
-            for service, service_detail in six.iteritems(
-                    region['current_usage']):
-                for resource, value in six.iteritems(service_detail):
+            for service, service_detail in region['current_usage'].items():
+                for resource, value in service_detail.items():
                     current_quota = region['current_quota'][service].get(
                         resource)
                     rows.append([service, resource, current_quota, value])
@@ -82,9 +80,9 @@ class QuotaSizes(command.Lister):
             'Size Name', 'Service', 'Resource', 'Value']
 
         rows = []
-        for size, size_details in six.iteritems(quota_data['quota_sizes']):
-            for service, service_details in six.iteritems(size_details):
-                for resource, value in six.iteritems(service_details):
+        for size, size_details in quota_data['quota_sizes'].items():
+            for service, service_details in size_details.items():
+                for resource, value in service_details.items():
                     rows.append([size, service, resource, value])
 
         return headers, rows

adjutantclient/v1/notifications.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from six.moves.urllib import parse
+from urllib import parse
 
 from adjutantclient.common import base
 

adjutantclient/v1/quota.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from six.moves.urllib import parse
+from urllib import parse
 
 from adjutantclient.common import base
 

adjutantclient/v1/roles.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from six.moves.urllib import parse
+from urllib import parse
 
 from adjutantclient.common import base
 from adjutantclient import exc

adjutantclient/v1/tasks.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from six.moves.urllib import parse
+from urllib import parse
 
 from adjutantclient.common import base
 

adjutantclient/v1/tokens.py
@@ -14,7 +14,7 @@
 
 from adjutantclient.common import base
 
-from six.moves.urllib import parse
+from urllib import parse
 
 
 class Token(base.Resource):

commit 5a92d900b9d3cb91b2ead16a299b61c0ac381653

Bugfix: Six removal introduced regression in v1/users
Bug introduced in b89a07b

Change-Id: I10ad2928f4faffb863152e0bbafc097b70b02082

adjutantclient/v1/users.py
@@ -13,7 +13,7 @@
 #    under the License.
 
 
-from moves.urllib import parse
+from urllib import parse
 
 from adjutantclient.common import base
 